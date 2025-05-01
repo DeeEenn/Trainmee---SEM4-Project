d59b383850390755e3af90a7692ec8b9d260611d
David Nic <david.nic@getcurium.com>
2025-05-01 22:18:57 +0100

    Update working User model backend

--- backend/src/main/java/com/treninkovydenik/treninkovy_denik/TreninkovyDenikApplication.java
+++ backend/src/main/java/com/treninkovydenik/treninkovy_denik/TreninkovyDenikApplication.java
@@ -1,13 +1,15 @@
-package com.treninkovydenik.treninkovy_denik;
+package cz.vutbr.fit.nicda1.trainingdiary;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class TreninkovyDenikApplication {
+public class TrainingDiaryApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TreninkovyDenikApplication.class, args);
+		SpringApplication.run(TrainingDiaryApplication.class, args);
 	}
-
 }
